fdd7f6f4856715bae76b86ac4308d30e0290e19f
Harry You <harryyhc@outlook.com>
2020-05-04 18:05:06 -0400

    lab 9

--- Models/Book.cs
+++ Models/Book.cs
@@ -9,5 +9,10 @@ namespace Fisher.Bookstore.Models
         public string Title { get; set; }
         public List<BookAuthor> BookAuthor { get; set; }
         public double Price { get; set; }
+
+        public void ApplyMemberDiscount()
+        {
+            Price = Price * .90;
+        }
     }
 }
